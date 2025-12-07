7b8b4a94b908a422961109c63513ff00433e1063
vickywasisht <vickywasisht@gmail.com>
2025-12-06 23:23:40 -0500

    literally changed everything

--- answers.py
+++ answers.py
@@ -9,60 +9,33 @@ class BST:
 	def __init__(self):
 		self.root = None
 		
-	def insert(self, value):
-		"""Helper method to insert values into BST for testing"""
-		if self.root is None:
-			self.root = self.Node(value)
-		else:
-			self._insert_helper(self.root, value)
-	
-	def _insert_helper(self, node, value):
-		if value < node.value:
-			if node.left is None:
-				node.left = self.Node(value)
-			else:
-				self._insert_helper(node.left, value)
-		elif value > node.value:  # Only insert if value is greater, ignore duplicates
-			if node.right is None:
-				node.right = self.Node(value)
-			else:
-				self._insert_helper(node.right, value)
-		# If value == node.value, do nothing (ignore duplicates)
-		
 	'''
 	This function prints every value in the tree that is between min and max inclusive. 
 	Function only visits a subtree where the values may be valid.
 	'''
-	def print_between(self, min, max): 
-		result = []
-		self._print_between_helper(self.root, min, max, result)
-		return result  # Return list for testing purposes
-	
-	def _print_between_helper(self, node, min_val, max_val, result):
-		if node is None:
-			return
+	def print_between(self, node, min, max): 
+		def _print_between(node, min, max):
+			if node is None:
+				return
+			else:
+				self.print_between(node.left, min, max)
+				if node.value >= min and node <= max:
+					print(str(node) + ", ")
+				self.print_between(node.right, min, max)
 		
-		# Only explore left subtree if current value is greater than min
-		if node.value > min_val:
-			self._print_between_helper(node.left, min_val, max_val, result)
-		
-		# Add current node if it's in range
-		if min_val <= node.value <= max_val:
-			result.append(node.value)
-		
-		# Only explore right subtree if current value is less than max
-		if node.value < max_val:
-			self._print_between_helper(node.right, min_val, max_val, result)
-	
+		_print_between(node, min, max)
+
 	# This function returns the height of the binary tree.
 	def height(self):
-		return self._height_helper(self.root)
+		return self.height_help(self.root)
 	
-	def _height_helper(self, node):
+	def height_help(self, node):
 		if node is None:
-			return 0
-		return 1 + max(self._height_helper(node.left), self._height_helper(node.right))
-	
+			return -1
+		left_height = self.height_help(node.left)
+		right_height = self.height_help(node.right)
+		return 1 + max(left_height, right_height)
+
 	'''
 	This method returns the inorder successor of value. 
 	If there are no nodes with this value in the BST, 
@@ -70,39 +43,24 @@ class BST:
 	this method should return None.
 	'''
 	def inorder_successor(self, value):
-		# First find the node with the given value
-		node = self._find_node(self.root, value)
-		if node is None:
-			return None
-		
-		# Case 1: Node has right subtree
-		if node.right is not None:
-			return self._find_min(node.right).value
-		
-		# Case 2: Node has no right subtree
-		# Find the deepest ancestor for which given node is in left subtree
-		successor = None
-		current = self.root
-		
-		while current is not None:
-			if value < current.value:
-				successor = current
+		def find_min(node):
+			current = node
+			while current.left is not None:
 				current = current.left
-			elif value > current.value:
-				current = current.right
+			return current
+
+		def find_successor(node, value):
+			if node is None:
+				return None
+
+			if value < node.value:
+				left = find_successor(node.left, value)
+				return left if left is not None else node
+			elif value > node.value:
+				return find_successor(node.right, value)
 			else:
-				break
-		
-		return successor.value if successor else None
-	
-	def _find_node(self, node, value):
-		if node is None or node.value == value:
-			return node
-		if value < node.value:
-			return self._find_node(node.left, value)
-		return self._find_node(node.right, value)
-	
-	def _find_min(self, node):
-		while node.left is not None:
-			node = node.left
-		return node
+				if node.right is not None:
+					return find_min(node.right)
+				return None
+
+		return find_successor(self.root, value)
--- work-here.py
+++ work-here.py
@@ -13,13 +13,29 @@ class BST:
 	This function prints every value in the tree that is between min and max inclusive. 
 	Function only visits a subtree where the values may be valid.
 	'''
-	def print_between(self, min, max): 
-		pass
-	
+	def print_between(self, node, min, max): 
+		def _print_between(node, min, max):
+			if node is None:
+				return
+			else:
+				self.print_between(node.left, min, max)
+				if node.value >= min and node <= max:
+					print(str(node) + ", ")
+				self.print_between(node.right, min, max)
+		
+		_print_between(node, min, max)
+
 	# This function returns the height of the binary tree.
 	def height(self):
-		pass
+		return self.height_help(self.root)
 	
+	def height_help(self, node):
+		if node is None:
+			return -1
+		left_height = self.height_help(node.left)
+		right_height = self.height_help(node.right)
+		return 1 + max(left_height, right_height)
+
 	'''
 	This method returns the inorder successor of value. 
 	If there are no nodes with this value in the BST, 
@@ -27,4 +43,24 @@ class BST:
 	this method should return None.
 	'''
 	def inorder_successor(self, value):
-		pass
+		def find_min(node):
+			current = node
+			while current.left is not None:
+				current = current.left
+			return current
+
+		def find_successor(node, value):
+			if node is None:
+				return None
+
+			if value < node.value:
+				left = find_successor(node.left, value)
+				return left if left is not None else node
+			elif value > node.value:
+				return find_successor(node.right, value)
+			else:
+				if node.right is not None:
+					return find_min(node.right)
+				return None
+
+		return find_successor(self.root, value)
